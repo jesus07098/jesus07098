f20b35d902d054c9f946dd22a8af60065e2cd8e6
jesus07098 <jesus07098@gmail.com>
2020-04-02 19:21:24 -0400

    delete casi hecho

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const Task = require("../models/task");
+
+router.get("/", async (req, res) => {
+  const tasks = await Task.find();
+  console.log(tasks);
+  res.render("index", {
+    tasks
+  });
+});
+
+router.post("/add", async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.redirect("/");
+});
+router.get("/delete/:_id", async (req, res) => {
+  //console.log(req.params)
+  const { id } = req.params;
+  await Task.remove({ _id: id });
+  res.redirect("/");
+});
+
+module.exports = router;
